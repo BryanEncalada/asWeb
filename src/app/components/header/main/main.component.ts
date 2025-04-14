3e217652affe6a4c9ed924a1b91695536963b83b
Franco Encalada <FrancoEncalada120@gmail.com>
2025-04-14 17:22:08 -0500

    ok

--- src/app/components/header/main/main.component.ts
+++ src/app/components/header/main/main.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
+import { PortalComponent } from '../portal/portal.component';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [MenuComponent, PortalComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
--- src/app/components/header/menu/menu.component.ts
+++ src/app/components/header/menu/menu.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
 }
