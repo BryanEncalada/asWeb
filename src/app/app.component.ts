85a0943bc289306519cde919f8fdf02674fe75e8
BryanEncalada <bryanencalada@gmail.com>
2025-04-15 16:37:13 -0500

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './components/header/main/main.component';
 import { MainFooterComponent } from './components/footer/main-footer/main-footer.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainComponent, MainFooterComponent],
+  imports: [RouterOutlet, MainComponent, MainFooterComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
