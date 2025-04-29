42130b0c8ad681caa53d3e7a2718c8dd655d259d
BryanEncalada <bryanencalada@gmail.com>
2025-04-28 23:26:15 -0500

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,30 +6,14 @@ import { MainFooterComponent } from './components/footer/main-footer/main-footer
 import { BannerComponent } from './components/banner/banner.component';
 
 import { MainNosotros2Component } from './components/nosotros2/main-nosotros2/main-nosotros2.component';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import { MainMaquinariasComponent } from './components/maquinarias/main-maquinarias/main-maquinarias.component';
-=======
 import { SupplyComponent } from './components/supply/supply.component';
->>>>>>> Stashed changes
-=======
-import { SupplyComponent } from './components/supply/supply.component';
->>>>>>> Stashed changes
 
 
 
 @Component({
   selector: 'app-root',
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  imports: [RouterOutlet, MainComponent, MainFooterComponent, MainNosotros2Component,BannerComponent, MainMaquinariasComponent],
-=======
   imports: [RouterOutlet, MainComponent, MainFooterComponent, MainNosotros2Component,BannerComponent, SupplyComponent],
->>>>>>> Stashed changes
-=======
-  imports: [RouterOutlet, MainComponent, MainFooterComponent, MainNosotros2Component,BannerComponent, SupplyComponent],
->>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
