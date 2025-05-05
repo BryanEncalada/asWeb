9e96d96d9e3a83d71502bb3d8fed781bd8cab34b
Franco <francoEncalada120@gmail.com>
2025-05-05 12:07:49 -0500

    Acordion falta boton de play

--- src/app/components/maquinarias/main-maquinarias/main-maquinarias.component.ts
+++ src/app/components/maquinarias/main-maquinarias/main-maquinarias.component.ts
@@ -14,11 +14,56 @@ export class MainMaquinariasComponent {
   activeFranjaIndex: number | null = null;
 
   franjas = [
-    { color: 'azul', imagen: 'assets/images/maquinarias/Recurso_10Maq.webp' },
-    { color: 'celeste', imagen: 'assets/images/maquinarias/Recurso_10Maq.webp' },
-    { color: 'negro', imagen: 'assets/images/maquinarias/Recurso_10Maq.webp' },
-    { color: 'mostaza', imagen: 'assets/images/maquinarias/Recurso_10Maq.webp' },
-    { color: 'rojo', imagen: 'assets/images/maquinarias/Recurso_10Maq.webp' } // nueva franja
+    {
+      color: 'azul',
+      imagen: 'assets/images/maquinarias/Capacitacion/Recurso57.webp',
+      tituloimg: 'assets/images/maquinarias/Capacitacion/Recurso58.webp',
+      textimg: 'assets/images/maquinarias/Capacitacion/Recurso55.webp',
+      booMostrtext: false,
+      style: {
+        'background-image': `linear-gradient(to bottom, rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0)), url('../../assets/images/maquinarias/Capacitacion/Recurso60.webp')`,
+      },
+    },
+    {
+      color: 'celeste',
+      imagen: 'assets/images/maquinarias/Diagnostico/Recurso43.webp',
+      tituloimg: 'assets/images/maquinarias/Diagnostico/Recurso46.webp',
+      textimg: 'assets/images/maquinarias/Diagnostico/Recurso44.webp',
+      booMostrtext: false,
+      style: {
+        'background-image': `linear-gradient(to bottom, rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0)), url('../../assets/images/maquinarias/Diagnostico/Recurso48.webp')`,
+      }
+    },
+    {
+      color: 'negro',
+      imagen: 'assets/images/maquinarias/mantenimientoPre/Recurso31.webp',
+      tituloimg: 'assets/images/maquinarias/mantenimientoPre/Recurso34.webp',
+      textimg: 'assets/images/maquinarias/mantenimientoPre/Recurso32.webp',
+      booMostrtext: false,
+      style: {
+        'background-image': `linear-gradient(to bottom, rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0)), url('../../assets/images/maquinarias/mantenimientoPre/Recurso36.webp')`,
+      }
+    },
+    {
+      color: 'mostaza',
+      imagen: 'assets/images/maquinarias/mantenimiento/Recurso67.webp',
+      tituloimg: 'assets/images/maquinarias/mantenimiento/Recurso70.webp',
+      textimg: 'assets/images/maquinarias/mantenimiento/Recurso68.webp',
+      booMostrtext: false,
+      style: {
+        'background-image': `linear-gradient(to bottom, rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0)), url('../../assets/images/maquinarias/mantenimiento/Recurso72.webp')`,
+      }
+    },
+    {
+      color: 'rojo',
+      imagen: 'assets/images/maquinarias/Arranque/Recurso81.webp',
+      tituloimg: 'assets/images/maquinarias/Arranque/Recurso82.webp',
+      textimg: 'assets/images/maquinarias/Arranque/Recurso79.webp',
+      booMostrtext: true,
+      style: {
+        'background-image': `linear-gradient(to bottom, rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0)), url('../../assets/images/maquinarias/Arranque/Recurso84.webp')`,
+      }
+    } // nueva franja
   ];
 
 
