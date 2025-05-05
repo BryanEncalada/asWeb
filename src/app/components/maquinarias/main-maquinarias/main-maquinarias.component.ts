bc97d2f4136260bb9ada2a818beb386fb16ca9d3
Franco Encalada <FrancoEncalada120@gmail.com>
2025-05-05 00:45:51 -0500

    Listas desplegables, falta cambiar el interior

--- src/app/components/maquinarias/main-maquinarias/main-maquinarias.component.ts
+++ src/app/components/maquinarias/main-maquinarias/main-maquinarias.component.ts
@@ -1,11 +1,30 @@
+
+import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-main-maquinarias',
-  imports: [],
+  imports: [NgFor, NgIf, NgStyle, NgClass],
   templateUrl: './main-maquinarias.component.html',
   styleUrl: './main-maquinarias.component.css'
 })
 export class MainMaquinariasComponent {
+
+  activeFranjaIndex: number | null = null;
+
+  franjas = [
+    { color: 'azul', imagen: 'assets/images/maquinarias/Recurso_10Maq.webp' },
+    { color: 'celeste', imagen: 'assets/images/maquinarias/Recurso_10Maq.webp' },
+    { color: 'negro', imagen: 'assets/images/maquinarias/Recurso_10Maq.webp' },
+    { color: 'mostaza', imagen: 'assets/images/maquinarias/Recurso_10Maq.webp' },
+    { color: 'rojo', imagen: 'assets/images/maquinarias/Recurso_10Maq.webp' } // nueva franja
+  ];
+
+
+  toggleFranja(index: number) {
+    this.activeFranjaIndex = this.activeFranjaIndex === index ? null : index;
+  }
+
 
 }
