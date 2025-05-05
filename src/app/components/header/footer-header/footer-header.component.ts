25a2977188ef11807bafe4a9130127483e572e64
Franco Encalada <FrancoEncalada120@gmail.com>
2025-05-04 22:21:51 -0500

    Portal

---
+++ src/app/components/header/footer-header/footer-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-header',
+  imports: [],
+  templateUrl: './footer-header.component.html',
+  styleUrl: './footer-header.component.css'
+})
+export class FooterHeaderComponent {
+
+}
--- src/app/components/header/main/main.component.ts
+++ src/app/components/header/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { PortalComponent } from '../portal/portal.component';
+import { FooterHeaderComponent } from '../footer-header/footer-header.component';
 
 @Component({
   selector: 'app-main',
-  imports: [MenuComponent, PortalComponent],
+  imports: [MenuComponent, PortalComponent, FooterHeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
