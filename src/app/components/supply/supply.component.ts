a7d38792167c361dee6dd5950c9f96798b7f6aeb
BryanEncalada <93891074+BryanEncalada@users.noreply.github.com>
2025-08-12 19:00:57 -0500

    espacio suppply

--- src/app/components/supply/supply.component.ts
+++ src/app/components/supply/supply.component.ts
@@ -37,58 +37,8 @@ import { ImgRigthComponent } from '../animation/img-rigth/img-rigth.component';
     ]),
   ],
 })
-export class SupplyComponent implements AfterViewInit {
-  isVisible = false;
-  isVisibleRight = false;
-
+export class SupplyComponent {
   imgLeft: string = 'assets/images/supply/Recurso 35.webp';
   img: string = 'assets/images/supply/Recurso 34.webp';
   txt: string = '';
-
-  @ViewChild('observedRightImg', { static: false })
-  observedRightImgRef!: ElementRef;
-
-  @ViewChild('observedImg', { static: false }) observedImgRef!: ElementRef;
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      // Observer para imagen izquierda
-      if (this.observedImgRef) {
-        const observer = new IntersectionObserver(
-          ([entry]) => {
-            if (entry.isIntersecting) {
-              console.log('Visible 1');
-              this.isVisible = true;
-              observer.unobserve(entry.target);
-            }
-          },
-          { threshold: 0.3 }
-        );
-
-        setTimeout(() => {
-          observer.observe(this.observedImgRef.nativeElement);
-        }, 100);
-      }
-
-      // Observer para imagen derecha
-      if (this.observedRightImgRef) {
-        const observerRight = new IntersectionObserver(
-          ([entry]) => {
-            if (entry.isIntersecting) {
-              console.log('Visible right');
-              this.isVisibleRight = true;
-              observerRight.unobserve(entry.target);
-            }
-          },
-          { threshold: 0.3 }
-        );
-
-        setTimeout(() => {
-          observerRight.observe(this.observedRightImgRef.nativeElement);
-        }, 100);
-      }
-    }
-  }
 }
